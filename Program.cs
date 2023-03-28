b65f917714c6b6cfeecc7f901c16a81d7fd7a42c
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-29 01:19:41 +0530

    push element to stack call UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welecom to the Stack And Queue Problem");
+
+            Console.WriteLine("#####################");
+
+            StackWithLinkedList obj = new StackWithLinkedList();
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            obj.Display();
         }
     }
 }
