721584e7adbe05aaff401a3a838c15dfeb14d654
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-29 01:23:36 +0530

    create stack using linked list

---
+++ StackWithLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class StackWithLinkedList
+    {
+        public Node top;
+        public StackWithLinkedList()
+        {
+            this.top = null;
+        }
+
+        public void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
