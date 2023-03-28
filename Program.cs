3c18455cc883e1d9cc9a41c16d522a6edb75dcf9
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-29 01:10:58 +0530

    Stack and Queue

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welecom to the Stack And Queue Problem");
+        }
+    }
+}
